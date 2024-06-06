8df8ce35d4bf2e54404918f3a93ee3e726f705ba
Hiep <tothanhhiep.work@gmail.com>
2024-06-06 22:14:15 +0700

    Auth Routes Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const singupUser = (req, res) =>{
+    console.log("Signed Up")
+}
+
+export const loginUser = (req, res) =>{
+    console.log("Logged in")
+}
+
+export const logoutUser = (req, res) =>{
+    console.log("Logged out")
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { loginUser, logoutUser, singupUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup");
-router.get("/login");
-router.get("/logout");
+router.get("/signup",singupUser);
+router.get("/login",loginUser);
+router.get("/logout",logoutUser);
 
 export default router;
