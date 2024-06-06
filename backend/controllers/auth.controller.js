0c51102b887f9f71e91108da5c6e156e498e2852
Hiep <tothanhhiep.work@gmail.com>
2024-06-06 23:27:34 +0700

    Logout Model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -53,24 +53,27 @@ export const signupUser = async (req, res) => {
   }
 };
 
-export const loginUser = async(req, res) => {
+export const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
-		const user = await User.findOne({ username });
-		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
 
-		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({ error: "Invalid username or password" });
-		}
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
 
-		generateTokenAndSetCookie(user._id, res);
+    generateTokenAndSetCookie(user._id, res);
 
-		res.status(200).json({
-			_id: user._id,
-			fullname: user.fullname,
-			username: user.username,
-			profilePic: user.profilePic,
-		});
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
   } catch (error) {
     console.log("Error in login controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
@@ -78,5 +81,11 @@ export const loginUser = async(req, res) => {
 };
 
 export const logoutUser = (req, res) => {
-  console.log("Logged out");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
