cdba72ab67ce41b58a94f021c1f05a181aa6af4d
Hiep <tothanhhiep.work@gmail.com>
2024-06-07 00:03:12 +0700

    Create Consersation Model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,7 +24,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
-});
+  // createdAt, updatedAt => Member since <createdAt>
+},
+{ timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
