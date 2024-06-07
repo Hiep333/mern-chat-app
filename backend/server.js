97b4f81193d1ae5d11120c6353dc29d58d8ef48a
Hiep <tothanhhiep.work@gmail.com>
2024-06-07 11:10:45 +0700

    Send Message EndPoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
