20ef9a61cc6bda51d0efeadf5e91ae90211b50d8
Hiep <tothanhhiep.work@gmail.com>
2024-06-07 16:31:17 +0700

    Protect route middleware and set up how to save conversation into database

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.status(401).json({ error: "Uauthorized - No Token Provided" });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ error: "Uauthorized - Invalid Token" });
+        };
+
+        const  user  = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ error: "Uauthorized - User Not Found" });
+        };
+
+        req.user = user;
+
+        next();       
+    } catch (error) {
+        console.log("Error in protectRoute middlewarw", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export default protectRoute;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 8228;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 
