b0338372bb7a35619d1ba4e51c8228b915bf9836
Hiep <tothanhhiep.work@gmail.com>
2024-06-08 23:02:11 +0700

    UI Design: Build SignUp Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from "../pages/login/Login"; // Đường dẫn đúng tới tệp Login.jsx
+import SignUp from "../pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <SignUp/>
     </div>
   );
 }
